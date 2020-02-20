c5373c886fab5404880771e6deba4107b0144e60
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2020-02-20 13:44:27 +0200

    put c.info behind `ergo.compiler.macro.log` property;

--- compiler/src/main/scala/org/ergoplatform/compiler/compilation/util.scala
+++ compiler/src/main/scala/org/ergoplatform/compiler/compilation/util.scala
@@ -4,6 +4,12 @@ import scala.reflect.macros.whitebox.{Context => MacroContext}
 
 object util {
 
+  private def variable(propName: String, envName: String, default: String) =
+    Option(System.getProperty(propName)).orElse(sys.env.get(envName)).getOrElse(default)
+
+  private[util] val debugEnabled =
+    variable("ergo.compiler.macro.log", "ergo_compiler_macro_log", "false").toBoolean
+
   implicit class RichContext(c: MacroContext) {
 
     def error(msg: String): Unit =
@@ -16,7 +22,7 @@ object util {
       c.warning(c.enclosingPosition, msg)
 
     def info(msg: String): Unit =
-      c.info(c.enclosingPosition, msg, force = true)
+      if (debugEnabled) c.info(c.enclosingPosition, msg, force = true)
 
     def debug[T](v: T): T = {
       info(v.toString)
